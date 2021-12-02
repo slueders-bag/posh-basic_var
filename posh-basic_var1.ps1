c4526ed230b59361c38d745f296af01acb6508be
Sven Lüders <84067492+slueders-bag@users.noreply.github.com>
2021-12-02 08:48:28 +0100

    Update posh-basic_var1.ps1

--- posh-basic_var1.ps1
+++ posh-basic_var1.ps1
@@ -1,21 +1,23 @@
 
-# Lab1
+# Last Logon
 $adm= Get-LocalUser administrator
 $adm.LastLogon
 $comp = (get-date) - ( $adm.LastLogon)
 $comp.days
+
 #oder
 (Get-Date).Subtract($adm.LastLogon) | select days
 
-# UTC
+# UTC conversion
 (get-date).ToUniversalTime()
-(get-date).AddHours(-1)
 ([System.DateTime]::UtcNow)
 
 #file copy
 $filecopy = Get-ChildItem C:\temp\mig.csv
 $filecopy.CopyTo("c:\sys\migcopy.csv")
+
 #oder
 Copy-Item $filecopy -Destination C:\sys\testcopy.csv
+
 #oder
 Start-BitsTransfer $filecopy -Destination C:\sys\testcopy.csv
